0e90d7967305ed746dabe7baaf4c0950369c2357
Andrew Lindley <andrew.lindley@ait.ac.at>
2014-11-11 10:18:22 +0100

    fix for broken build

--- backmeup-indexer-core/src/test/java/org/backmeup/index/IndexFragmentSharingTests.java
+++ backmeup-indexer-core/src/test/java/org/backmeup/index/IndexFragmentSharingTests.java
@@ -1,84 +1,67 @@
 package org.backmeup.index;
 
-import java.io.IOException;
-import java.util.UUID;
-
-import org.backmeup.index.db.RunningIndexUserConfig;
 import org.junit.Rule;
-import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 /**
- * Contains tests for sharing index fragments with other users - Includes the
- * physical data operations (copying IndexDocuments, etc) - Importing and
- * deleting fragments from Elasticsearch itself - rebuilding an ES index from
+ * Contains tests for sharing index fragments with other users - Includes the physical data operations (copying
+ * IndexDocuments, etc) - Importing and deleting fragments from Elasticsearch itself - rebuilding an ES index from
  * scratch (file basis)
  */
 @RunWith(JUnit4.class)
 public class IndexFragmentSharingTests extends IndexManagerSetup {
 
-	// TODO Add Startup Class for all Integration Tests
-	// TODO @Before -> Put IndexFragment to-import
-	// TODO activateverifyIndexBuiltProperlyFromFragments
+    // TODO Add Startup Class for all Integration Tests
+    // TODO @Before -> Put IndexFragment to-import
+    // TODO activateverifyIndexBuiltProperlyFromFragments
 
-	@Rule
-	public ExpectedException exception = ExpectedException.none();
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
 
-	private RunningIndexUserConfig startupInstance(int userID)
-			throws NumberFormatException, ExceptionInInitializerError,
-			IllegalArgumentException, IOException, InterruptedException {
+    /* private RunningIndexUserConfig startupInstance(int userID) throws IndexManagerCoreException, IOException {
 
-		this.indexManager.startupInstance(userID);
-		RunningIndexUserConfig conf = this.indexManager
-				.getRunningIndexUserConfig(userID);
+         this.indexManager.startupInstance(userID);
+         RunningIndexUserConfig conf = this.indexManager.getRunningIndexUserConfig(userID);
 
-		int httpPort = conf.getHttpPort();
-		String drive = conf.getMountedTCDriveLetter();
+         int httpPort = conf.getHttpPort();
+         String drive = conf.getMountedTCDriveLetter();
 
-		System.out.println("user " + userID + " on port: " + httpPort
-				+ " and TC volume: " + drive);
-		// check instance up and running
-		ESConfigurationHandler.isElasticSearchInstanceRunning(
-				conf.getHostAddress(), httpPort);
+         System.out.println("user " + userID + " on port: " + httpPort + " and TC volume: " + drive);
+         // check instance up and running
+         ESConfigurationHandler.isElasticSearchInstanceRunning(conf.getHostAddress(), httpPort);
 
-		return conf;
-	}
+         return conf;
+     }*/
 
-	@Test
-	public void importIndexFragmentsToES() {
-		this.exception.expect(IllegalArgumentException.class);
-		UUID uuid = UUID.fromString("Test");
-	}
-
-	/*
-	 * @Test
-	 * 
-	 * @Ignore public void verifyIndexBuiltProperlyFromFragments() {
-	 * System.out.println("Verifying indexing content"); Client client =
-	 * node.client(); SearchResponse response =
-	 * rawClient.prepareSearch("backmeup")
-	 * .setQuery(QueryBuilders.matchAllQuery()).execute().actionGet();
-	 * SearchHits hits = response.getHits(); Assert.assertEquals(3,
-	 * hits.getHits().length);
-	 * 
-	 * for (SearchHit hit : response.getHits().getHits()) { Map<String, Object>
-	 * source = hit.getSource(); for (String key : source.keySet()) {
-	 * System.out.println(key + ": " + source.get(key));
-	 * 
-	 * if (key.equals("owner_name")) Assert.assertEquals("john.doe",
-	 * source.get(key));
-	 * 
-	 * if (key.equals("owner_id")) Assert.assertEquals(1, source.get(key));
-	 * 
-	 * if (key.equals("backup_sources"))
-	 * Assert.assertEquals("org.backmeup.dummy", source.get(key));
-	 * 
-	 * if (key.equals("indexrecord_uuid"))
-	 * Assert.assertEquals("NEED TO ADD UUID HERE", source.get(key)); } }
-	 * 
-	 * System.out.println("Done."); }
-	 */
+    /*
+     * @Test
+     * 
+     * @Ignore public void verifyIndexBuiltProperlyFromFragments() {
+     * System.out.println("Verifying indexing content"); Client client =
+     * node.client(); SearchResponse response =
+     * rawClient.prepareSearch("backmeup")
+     * .setQuery(QueryBuilders.matchAllQuery()).execute().actionGet();
+     * SearchHits hits = response.getHits(); Assert.assertEquals(3,
+     * hits.getHits().length);
+     * 
+     * for (SearchHit hit : response.getHits().getHits()) { Map<String, Object>
+     * source = hit.getSource(); for (String key : source.keySet()) {
+     * System.out.println(key + ": " + source.get(key));
+     * 
+     * if (key.equals("owner_name")) Assert.assertEquals("john.doe",
+     * source.get(key));
+     * 
+     * if (key.equals("owner_id")) Assert.assertEquals(1, source.get(key));
+     * 
+     * if (key.equals("backup_sources"))
+     * Assert.assertEquals("org.backmeup.dummy", source.get(key));
+     * 
+     * if (key.equals("indexrecord_uuid"))
+     * Assert.assertEquals("NEED TO ADD UUID HERE", source.get(key)); } }
+     * 
+     * System.out.println("Done."); }
+     */
 
 }
