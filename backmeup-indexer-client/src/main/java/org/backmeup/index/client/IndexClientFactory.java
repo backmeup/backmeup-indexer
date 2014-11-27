974fb5c4de6446c844e6d10115ac244ed9ff7d43
Andrew Lindley <andrew.lindley@ait.ac.at>
2014-11-27 14:02:05 +0100

    cut dependency to indexer core and enable usage of rest client instead of the elastic client

--- backmeup-indexer-client/src/main/java/org/backmeup/index/client/IndexClientFactory.java
+++ backmeup-indexer-client/src/main/java/org/backmeup/index/client/IndexClientFactory.java
@@ -1,12 +1,12 @@
 package org.backmeup.index.client;
 
-import org.backmeup.data.dummy.ElasticSearchIndexClient;
 import org.backmeup.index.api.IndexClient;
+import org.backmeup.index.client.rest.RestApiIndexClient;
 
 public class IndexClientFactory {
 
     public IndexClient getIndexClient(Long userId) {
-        return new ElasticSearchIndexClient(userId);
+        return new RestApiIndexClient(userId);
     }
 
 }
