bbe41a25b60126d2a72cc961098aace8838ec65b
Andrew Lindley <andrew.lindley@ait.ac.at>
2014-11-27 15:20:45 +0100

    read output of commands only on linux platform

--- backmeup-indexer-core/src/main/java/org/backmeup/index/TCMountHandler.java
+++ backmeup-indexer-core/src/main/java/org/backmeup/index/TCMountHandler.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -93,6 +92,7 @@ public class TCMountHandler {
             if (isDriveMounted(driveLetter)) {
                 break;
             }
+            log.debug("waiting for mount of drivletter: " + driveLetter + " " + i + ".time");
             Thread.sleep(1000 / 2);
         }
 
@@ -112,20 +112,23 @@ public class TCMountHandler {
             // Execute the call
             log.debug("executing: " + command);
             Process process = Runtime.getRuntime().exec(command);
-            // TODO ProcessBuilder
-            Reader r = new InputStreamReader(process.getInputStream());
-            BufferedReader in = new BufferedReader(r);
-            String line;
-            String result = "";
+            // TODO ProcessBuilder?
 
-            // TODO int errorCode = process.waitFor();
-            // TODO Andrew, use the exit code if maybe it is ok
-            Thread.sleep(100);
-
-            while ((line = in.readLine()) != null) {
-                result += line;
+            if (SystemUtils.IS_OS_LINUX) {
+                log.debug("checking Output of command");
+                try (BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                    String result = "";
+                    String line;
+                    while ((line = in.readLine()) != null) {
+                        result += line;
+                    }
+                    log.debug(result);
+                }
             }
-            log.debug(result);
+
+            log.debug("waiting for command t finish");
+            int errorCode = process.waitFor();
+            // TODO Andrew, use the exit code if maybe it is ok
 
         } catch (IOException | InterruptedException e) {
             log.error("Error executing: " + command + " " + e.toString());
