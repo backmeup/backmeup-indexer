7b2afca12c4b2a8ffb7ca65b41719ed58641c9f8
Andrew Lindley <andrew.lindley@ait.ac.at>
2014-11-27 13:23:39 +0100

    comment test "work in progress"

--- backmeup-indexer-core/src/test/java/org/backmeup/index/IndexFragmentImportTests.java
+++ backmeup-indexer-core/src/test/java/org/backmeup/index/IndexFragmentImportTests.java
@@ -1,9 +1,6 @@
 package org.backmeup.index;
 
-import java.net.URL;
-
-import org.backmeup.index.db.RunningIndexUserConfig;
-import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -14,8 +11,9 @@ public class IndexFragmentImportTests extends IndexManagerSetup {
     //TODO Datei händisch im Workspace anlegen oder mit UnitTest hin kopieren 
 
     @Test
+    @Ignore
     public void importUserOwnedIndexFragmentsInES() {
-        this.indexManager.startupInstance(999991);
+        /*this.indexManager.startupInstance(999991);
         System.out.println("startup done");
 
         RunningIndexUserConfig conf = this.indexManager.getRunningIndexUserConfig(999991);
@@ -23,7 +21,7 @@ public class IndexFragmentImportTests extends IndexManagerSetup {
         int httpPort = conf.getHttpPort();
         URL host = conf.getHostAddress();
         Assert.assertTrue("ES instance up and running?",
-                ESConfigurationHandler.isElasticSearchInstanceRunning(host, httpPort));
+                ESConfigurationHandler.isElasticSearchInstanceRunning(host, httpPort));*/
     }
 
 }
