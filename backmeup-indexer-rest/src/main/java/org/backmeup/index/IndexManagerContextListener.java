f134cd5d6a9daa3b31df63e43439645eadfb8c28
Peter Kofler <peter.kofler@code-cop.org>
2014-12-02 19:50:04 +0100

    move context listener to rest module, remove Servlet dependency in core module.

--- backmeup-indexer-rest/src/main/java/org/backmeup/index/IndexManagerContextListener.java
+++ backmeup-indexer-rest/src/main/java/org/backmeup/index/IndexManagerContextListener.java
@@ -6,35 +6,32 @@ import javax.servlet.ServletContextListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/*
- * TODO currently not active, as not deployed as servlet
- * change deployment unit to war, add web.xml
- * @see http://stackoverflow.com/questions/3838217/using-cdi-weld-in-tomcat-with-simple-pojo-classes 
- */
-
 public class IndexManagerContextListener implements ServletContextListener {
-    private final Logger log = LoggerFactory.getLogger(IndexManagerContextListener.class);
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     @Override
-    public void contextInitialized(ServletContextEvent arg0) {
+    public void contextInitialized(ServletContextEvent event) {
         this.log.debug(">>>>> Startup IndexManager >>>>>");
         try {
             IndexManager im = IndexManager.getInstance();
             im.startupIndexManager();
-        } catch (Exception ex) {
-            this.log.error("Error (nothing injected?)", ex);
+        } catch (RuntimeException ex) {
+            this.log.error("Error", ex);
+            throw ex;
         }
         this.log.debug(">>>>> Startup IndexManager DONE >>>>>");
     }
 
     @Override
-    public void contextDestroyed(ServletContextEvent arg0) {
+    public void contextDestroyed(ServletContextEvent event) {
         this.log.debug(">>>>> Shutdown IndexManager >>>>>");
         try {
             IndexManager im = IndexManager.getInstance();
             im.shutdownIndexManager();
-        } catch (Exception ex) {
-            this.log.error("Error (nothing injected?)", ex);
+        } catch (RuntimeException ex) {
+            this.log.error("Error", ex);
+            throw ex;
         }
         this.log.debug(">>>>> Shutdown IndexManager DONE >>>>>");
     }
