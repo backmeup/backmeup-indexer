00432e196d2590ac4a283cb78f5e909b46e43abd
Peter Kofler <peter.kofler@code-cop.org>
2014-12-12 20:36:13 +0100

    The GC task needs its own transaction because this is a background thread

--- backmeup-indexer-core/src/main/java/org/backmeup/index/IndexGarbageCollectionTask.java
+++ backmeup-indexer-core/src/main/java/org/backmeup/index/IndexGarbageCollectionTask.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+import org.backmeup.index.dal.Transaction;
 import org.backmeup.index.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,24 +14,39 @@ import org.slf4j.LoggerFactory;
 public class IndexGarbageCollectionTask implements Runnable {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
-    
+
     @Inject
     private IndexManager indexManager;
     @Inject
     private IndexKeepAliveTimer indexKeepAliveTimer;
 
+    // needs its own transaction because this is a background thread
+    @Inject
+    private Transaction transaction;
+
     @Override
     public void run() {
+        transaction.inside(new Runnable() {
+            @Override
+            public void run() {
+                runWithTransaction();
+            }
+        });
+    }
+
+    private void runWithTransaction() {
         log.debug("started running garbage collection for ElasticSearch Instances no longer in use.");
         List<User> userIDs = indexKeepAliveTimer.getUsersToShutdown();
-        
+
         int openInstances = indexKeepAliveTimer.countOpenInstances();
         log.debug("Open ES instances: " + openInstances + " marked instances for shutdown: " + userIDs.size());
 
         for (User userId : userIDs) {
             log.info("IndexCoreGarbageCollector executing shutdown for userID: " + userId);
+
             //iterate over all instances to shutdown
-            indexManager.shutdownInstance(userId); 
+            indexManager.shutdownInstance(userId);
+
             //flag them as done within the timer
             indexKeepAliveTimer.flagAsShutdown(userId);
         }
