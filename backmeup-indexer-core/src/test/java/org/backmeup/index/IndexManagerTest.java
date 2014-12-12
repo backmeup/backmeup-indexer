dcea71ab3e99c2fbdb807d032a4824c843f4d62e
Peter Kofler <peter.kofler@code-cop.org>
2014-12-12 17:42:44 +0100

    remove unused imports

--- backmeup-indexer-core/src/test/java/org/backmeup/index/IndexManagerTest.java
+++ backmeup-indexer-core/src/test/java/org/backmeup/index/IndexManagerTest.java
@@ -33,8 +33,6 @@ import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 
 public class IndexManagerTest extends IndexManagerSetup {
 
