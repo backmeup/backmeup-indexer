fc5be3270873b640c737f8c4d3ad0a508b83fc64
Peter Kofler <peter.kofler@code-cop.org>
2014-12-13 18:46:01 +0100

    createRandom but valid Config And Mark It As Accessed In KeepAliveTimer

--- backmeup-indexer-rest/src/main/java/org/backmeup/index/rest/resources/Config.java
+++ backmeup-indexer-rest/src/main/java/org/backmeup/index/rest/resources/Config.java
@@ -1,6 +1,5 @@
 package org.backmeup.index.rest.resources;
 
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Random;
 
@@ -10,6 +9,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.backmeup.index.IndexKeepAliveTimer;
+import org.backmeup.index.UserDataWorkingDir;
+import org.backmeup.index.core.elasticsearch.SearchInstances;
 import org.backmeup.index.core.model.RunningIndexUserConfig;
 import org.backmeup.index.dal.RunningIndexUserConfigDao;
 import org.backmeup.index.model.User;
@@ -19,31 +21,39 @@ import org.backmeup.index.serializer.Json;
 @Produces(MediaType.APPLICATION_JSON)
 public class Config {
 
+    private final Random random = new Random();
     @Inject
     private RunningIndexUserConfigDao dao;
+    @Inject
+    private SearchInstances searchInstance;
+    @Inject
+    private IndexKeepAliveTimer indexKeepAliveTimer;
 
     @GET
     @Path("/all")
-    public String all() {
+    public String showAllConfigEntriesInDb() {
         return Json.serialize(dao.getAllESInstanceConfigs());
     }
 
     @GET
     @Path("/random")
-    public String createRandom() throws MalformedURLException {
+    public String createRandomConfigAndMarkItAsAccessedInKeepAliveTimer() {
         RunningIndexUserConfig config = createRandomConfig();
+
         dao.save(config);
+        indexKeepAliveTimer.extendTTL20(config.getUser());
+
         return Json.serialize(config);
     }
 
-    private RunningIndexUserConfig createRandomConfig() throws MalformedURLException {
-        Random random = new Random();
+    private RunningIndexUserConfig createRandomConfig() {
 
         User userId = new User(Math.abs(random.nextLong()));
-        URL hostUrl = new URL("http://1.com/");
+        URL hostUrl = searchInstance.getDefaultHost();
         int port = random.nextInt(65534);
         String clusterName = "user" + userId;
-        String letter = "" + ('a' + random.nextInt(26));
-        return new RunningIndexUserConfig(userId, hostUrl, port, port + 1, clusterName, letter, "/tmp");
+        String letter = "" + ((char)('a' + random.nextInt(26)));
+        String dataSink = UserDataWorkingDir.getDir(userId);
+        return new RunningIndexUserConfig(userId, hostUrl, port, port + 1, clusterName, letter, dataSink);
     }
 }
